2008d59863c101b1f1949ec263dbbd4f9ce2c766
NausheenRoy <75558008+NausheenRoy@users.noreply.github.com>
2021-12-26 15:28:26 +0530

    Add files via upload

---
+++ C132.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("stars_with_gravity.csv")
+
+
+mass = df["Mass"].to_list()
+radius = df["Radius"].to_list()
+gravity = df["Gravity"].to_list()
+
+mass.sort()
+radius.sort()
+gravity.sort()
+plt.plot(radius,mass)
+
+
+plt.title("Radius & Mass of the Star")
+plt.xlabel("Radius")
+plt.ylabel("Mass")
+plt.show()
+
+plt.plot(gravity,mass)
+plt.title("gravity & Mass of the Star")
+plt.xlabel("gravity")
+plt.ylabel("Mass")
+plt.show()
+
